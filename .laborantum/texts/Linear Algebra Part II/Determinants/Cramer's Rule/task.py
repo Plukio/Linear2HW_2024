aeeb2d558b9fe7a107823809872df21b13a6a1c3
Pluk170 <112405935+Pluk170@users.noreply.github.com>
2025-01-09 18:28:27 +0000

    Submit

--- .laborantum/texts/Linear Algebra Part II/Determinants/Cramer's Rule/task.py
+++ .laborantum/texts/Linear Algebra Part II/Determinants/Cramer's Rule/task.py
@@ -10,61 +10,85 @@ answer = {
     "task1": {
         "det_A_terms": sorted([
             ### YOUR CODE HERE ###
+            [1, -1]
         ]),
         "det_A_1_terms": sorted([
             ### YOUR CODE HERE ###
+            [-2.0, 0]
         ]),
         "det_A_2_terms": sorted([
             ### YOUR CODE HERE ###
+            [-2, 0]
         ]),
         "x": np.array(
             ### YOUR CODE HERE ###
+            [1, 1]
         ),
         "cofactors": np.array(
             ### YOUR CODE HERE ###
+            [[-1.0, -1.0], 
+            [-1.0, 1.0]]
         ),
         "inverse": np.array(
             ### YOUR CODE HERE ###
+            [[0.5, 0.5], 
+            [0.5, -0.5]]
         )
     },
     "task2": {
         "det_A_terms": sorted([
             ### YOUR CODE HERE ###
+            [2, 3]
         ]),
         "det_A_1_terms": sorted([
             ### YOUR CODE HERE ###
+            [2, 8]
         ]),
         "det_A_2_terms": sorted([
             ### YOUR CODE HERE ###
+            [-3, 8]
         ]),
         "x": np.array(
             ### YOUR CODE HERE ###
+            [2.0, 1.0]
         ),
         "cofactors": np.array(
             ### YOUR CODE HERE ###
+            [[2.0, -3.0], [1.0, 1.0]]
         ),
         "inverse": np.array(
             ### YOUR CODE HERE ###
+            [[0.4, 0.2], [-0.6, 0.2]]
         )
     },
     "task3": {
         "det_A_terms": sorted([
             ### YOUR CODE HERE ###
+            [-6, -2, -2, 1, 2, 3]
         ]),
         "det_A_1_terms": sorted([
             ### YOUR CODE HERE ###
+            [-12, -4, 0, 0, 3, 9]
         ]),
         "det_A_2_terms": sorted([
             ### YOUR CODE HERE ###
+            [-9, -4, 0, 0, 3, 6]
         ]),
         "x": np.array(
             ### YOUR CODE HERE ###
+            [1, 1.0, -0.0]
         ),
         "cofactors": np.array(
             ### YOUR CODE HERE ###
+            [[-8, 1.0, 3],
+            [-0.0, 1.0, -1.0],
+            [4.0, -2.0, -2.0]]
         ),
         "inverse": np.array(
             ### YOUR CODE HERE ###
+            [[2.0, 0.0, -1.0],
+             [-0.25, -0.25, 0.5],
+             [-0.75, 0.25, 0.5]]
         )
     }
 }
